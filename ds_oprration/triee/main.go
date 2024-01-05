f17ff5e47972ea23a082a46f010d16cb3b17ead4
ashikask2002 <ksashik999@gmail.com>
2024-01-05 20:23:41 +0530

    triee operatoin

---
+++ ds_oprration/triee/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+const size = 26
+
+type Node struct {
+	children [size]*Node
+	isend    bool
+}
+
+type trie struct {
+	root *Node
+}
+
+func (t *trie) Insert(value string) {
+	n := len(value)
+	current := t.root
+	for i := 0; i < n; i++ {
+		curnInds := value[i] - 'a'
+		if current.children[curnInds] == nil {
+			current.children[curnInds] = &Node{}
+		}
+		current = current.children[curnInds]
+	}
+	current.isend = true
+
+}
+
+func (t *trie) Search(value string) bool {
+	n := len(value)
+	current := t.root
+	for i := 0; i < n; i++ {
+		curnInds := value[i] - 'a'
+		if current.children[curnInds] == nil {
+			return false
+		}
+		current = current.children[curnInds]
+	}
+	if current.isend == true {
+		return true
+	}
+	return false
+}
+
+func main() {
+	t := &trie{root: &Node{}}
+	toAdd := []string{
+		"ashik",
+		"ashi",
+		"ash",
+	}
+	for _, val := range toAdd {
+		t.Insert(val)
+	}
+	b := t.Search("ashik")
+	fmt.Println(" ", b)
+	j := t.Search("h")
+	fmt.Println(" ",j)
+}
