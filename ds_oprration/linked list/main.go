bed50242c47da2e74131513245ee9060f63965d7
ashikask2002 <ksashik999@gmail.com>
2024-01-06 21:05:57 +0530

    added the addend

--- ds_oprration/linked list/main.go
+++ ds_oprration/linked list/main.go
@@ -12,24 +12,42 @@ type LinkedList struct {
 }
 
 func main() {
-	list := LinkedList{}
-	list.addHead(1)
-	list.addHead(5)
-	list.addHead(3)
-	list.addHead(9)
-	list.displayElements()
+	 list := LinkedList{}
+	// list.addHead(1)
+	// list.addHead(5)
+	// list.addHead(3)
+	// list.addHead(9)
+list.addend(5)
+list.addend(3)
+list.addend(7)
+list.addend(2)
+	 list.displayElements()
 
 }
 
-func (l *LinkedList) addHead(value int) {
+// func (l *LinkedList) addHead(value int) {
+// 	node := &Node{value, nil}
+
+// 	if l.head == nil {
+// 		l.head = node
+
+// 	} else {
+// 		node.next = l.head
+// 		l.head = node
+// 	}
+// }
+
+func (l *LinkedList) addend(value int) {
 	node := &Node{value, nil}
+	temp := l.head
 
-	if l.head == nil {
+	if temp == nil {
 		l.head = node
-
 	} else {
-		node.next = l.head
-		l.head = node
+		for temp.next != nil {
+			temp = temp.next
+		}
+		temp.next = node
 	}
 }
 
