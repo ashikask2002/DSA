a1e8b0f27330410224b0bd57005d74371ffba8ba
ashikask2002 <ksashik999@gmail.com>
2024-01-07 23:44:34 +0530

    added append,deleteindex and reverse

--- ds_oprration/array/main.go
+++ ds_oprration/array/main.go
@@ -23,8 +23,6 @@
 // 	}
 // }
 
-
-
 package main
 
 import "fmt"
@@ -35,13 +33,19 @@ func main() {
 	size := 5
 
 	Addarray(arr, size)
-	insertintOarray(arr,size,2,9)
+	// insertintOarray(arr,size,2,9)
+	// Displayarray(arr, size)
+
+	// append(arr, size, 11)
+
+	// deleteindex(arr,size,3)
+	ReverseArray(arr,size)
+
 	Displayarray(arr, size)
+
 }
 
 // --------add the elements into the array-------
-
-
 
 func Addarray(arr []int, size int) {
 	for i := 0; i < size; i++ {
@@ -56,13 +60,40 @@ func Displayarray(arr []int, size int) {
 	}
 }
 
-
 // ----- insert into an array-------
 
-func  insertintOarray(arr []int, size int, index int, value int){
-	for j := size; j > index; j--{
+func insertintOarray(arr []int, size int, index int, value int) {
+	for j := size; j > index; j-- {
 		arr[j] = arr[j-1]
 	}
 	arr[index] = value
 
+}
+
+// ------append-------//
+func append(arr []int, size int, value int) {
+	arr[size-1] = value
+	fmt.Println(value)
+
+}
+
+// --------deleteindex-------
+func deleteindex(arr []int, size int, index int) {
+	for i := 0; i < size; i++ {
+		if i == index {
+			for j := i; j < size; j++ {
+				arr[j] = arr[j+1]
+			}
+			size = size - 1
+		}
+	}
+}
+
+func ReverseArray(arr[]int,size int){
+	var temp int
+	for i,j := 0,size-1; i<j;i,j = i+1,j-1{
+		temp = arr[i]
+		arr[i]=arr[j]
+		arr[j]=temp
+	}
 }
