01ef88cadce4e8a5797bf5e5444548b97efb0ea9
ashikask2002 <ksashik999@gmail.com>
2024-01-13 13:41:15 +0530

    discending oroder in bbblesort

---
+++ ds_oprration/sort/Bubblesort/descending/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 5, 2, 7, 4, 3}
+	fmt.Println("array before sorting", arr)
+
+	result := BubbleSort(arr)
+	fmt.Println("array after sorting", result)
+}
+
+func BubbleSort(arr []int) []int {
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		flag := 0
+		for j := 0; j < n-1-i; j++ {
+			if arr[j] < arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				flag = 1
+			}
+		}
+		if flag == 0 {
+			break
+		}
+
+	}
+	return arr
+}
