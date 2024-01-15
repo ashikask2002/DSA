21b41a5ef205dddce6ca794d6f7f2e8d29c4e570
ashikask2002 <ksashik999@gmail.com>
2024-01-15 13:18:21 +0530

    quick sort ascending

---
+++ ds_oprration/sort/quickSort/descending /main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{2, 5, 1, 3, 7, 4, 6}
+	fmt.Println("array before sorting", arr)
+	start := 0
+	n := len(arr)-1
+	end := n
+	result := QuickSort(arr, start, end)
+	fmt.Println("array after sorting", result)
+}
+
+func QuickSort(arr []int,start,end int)[]int{
+	if start < end {
+		loc := partition(arr,start,end)
+
+		QuickSort(arr,start,loc-1)
+		QuickSort(arr,loc+1,end)
+	}
+	return arr
+}
+
+func partition(arr []int,start,end int)int{
+	pivot := arr[start]
+	i := start+1
+	j := end
+	for i<j{
+		for arr[i]>= pivot && i < end{
+			i++
+		}
+		for arr[j] < pivot{
+			j--
+		}
+		if i <j{
+			arr[i],arr[j] = arr[j],arr[i]
+		}
+	}
+	arr[start],arr[j] = arr[j],arr[start]
+	return j
+}
