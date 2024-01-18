49e288d9dbe7ca8ec3f38e40e4fd2bedc1cd0fb6
ashikask2002 <ksashik999@gmail.com>
2024-01-18 20:05:13 +0530

    merge sort descending

---
+++ ds_oprration/sort/mergesort/descending order/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{6, 1, 9, 3, 5, 2}
+	fmt.Println("array before sorting", arr)
+	start := 0
+	end := len(arr) - 1
+	result := MergeSort(arr,start, end)
+	fmt.Println("array after sorting", result)
+}
+
+
+func MergeSort(arr []int,start int,end int)[]int{
+	if start < end{
+		mid := (start + end )/2
+		 MergeSort(arr,start,mid)
+		 MergeSort(arr,mid+1,end)
+		 Merge(arr,start,mid,end)
+	}
+	return arr
+}
+
+func Merge(arr []int,start int, mid int, end int){
+	i :=  start
+	j := mid +1
+    var b []int
+	for  i <= mid && j <= end{
+     if arr[i] >= arr[j]{
+		b = append(b,arr[i])
+		i++
+	 }else{
+		b = append(b,arr[j])
+		j++
+	 }      
+
+	}
+
+	for ; i <= mid ;i++{
+		b = append(b,arr[i])
+	}
+	for ; j <= end ;j++{
+		b = append(b,arr[j])
+	}
+
+	for i := start ; i<= end; i++{
+		arr[i]= b[i-start]
+	}
+}
