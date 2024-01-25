6e671564c7deab16a801fe7fdb68b0abe0a53b9e
ashikask2002 <ksashik999@gmail.com>
2024-01-25 14:34:13 +0530

    find particular element from stack

---
+++ ds_oprration/stack/removeparticular/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Stack struct {
+	data []int
+	size int
+}
+
+func main() {
+	l := &Stack{}
+	l.Push(3)
+	l.Push(2)
+	l.Push(6)
+	l.Push(1)
+	l.Push(8)
+	fmt.Println("array before ", l.data)
+	value := 2
+	l.removeElement(value)
+	fmt.Println("array after", l.data)
+}
+
+func (s *Stack) Push(a int) {
+	s.data = append(s.data, a)
+	s.size++
+}
+
+func (s *Stack) pop() int {
+	if s.size == 0 {
+		return -1 
+	}
+	n := len(s.data) - 1
+	toremove := s.data[n]
+	s.data = s.data[:n]
+	s.size--
+	return toremove
+}
+
+func (s *Stack)removeElement(a int){
+	var nestack Stack
+
+	for s.size > 0{
+       temp := s.pop()
+	   if temp != a{
+		nestack.Push(temp)
+	   }
+	}
+	for nestack.size > 0{
+		s.Push(nestack.pop())
+	}
+}
